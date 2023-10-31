faf40ab80e29b2915a8e46a639a08c1b8a5fffc3
MuhammadArkan <muhammadarkan@graduate.utm.my>
2023-10-31 10:30:12 +0700

    convert infotab datas into json

--- InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/InfoTab/InfoTab.jsx
+++ InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/InfoTab/InfoTab.jsx
@@ -1,42 +1,31 @@
 import "../../../../../App.css";
-import React, { useEffect, useState } from 'react';
-import UTTerminalImage from "../../../../Img/UT-terminalBatam.jpeg"; // Make sure to adjust the image path
-
+import React, { useEffect, useState } from "react";
+import UTTerminalImage from "../../../../Img/UT-terminalBatam.jpeg";
 
 function InfoTab() {
-  
-  const data = {
-    error: "",
-    data: {
-      serialNumber: "21163457",
-      terminalType: "Inmarsat-C Transceiver",
-      mobileNumber: "400022211",
-      mobileType: "TT-3027C SOLAS Maritime",
-      ISNnumber: "4TT094ac823c",
-      HWid: "1",
-      latitude: "01 04 95 N",
-      longitude: "104 01 76 E",
-      altitude: "410",
-      GPStime: "21:04:05 UTC",
-      isSerialConnected: "True",
-      hostUptime: 42498717,
-      applicationUptime: 19848069,
-    },
-  };
+  const [jsonData, setJsonData] = useState(null);
 
-  const serialNumber = data.data.serialNumber;
-  const terminalType = data.data.terminalType;
-  const mobileNumber = data.data.mobileNumber;
-  const mobileType = data.data.mobileType;
-  const ISNnumber = data.data.ISNnumber;
-  const HWid = data.data.HWid;
-  const latitude = data.data.latitude;
-  const longitude = data.data.longitude;
-  const altitude = data.data.altitude;
-  const GPStime = data.data.GPStime;
-  const isSerialConnected = data.data.isSerialConnected;
-  const hostUptime = data.data.hostUptime;
-  const applicationUptime = data.data.applicationUptime;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "/datas/infotabData/infotabbatamdata.json"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setJsonData(data);
+      } catch (error) {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="contents">
@@ -51,60 +40,16 @@ function InfoTab() {
       <div className="content">
         <div className="head-content">Device Information</div>
         <div>
-          <table className="tbl">
-            <tr>
-              <th>Serial Number</th>
-              <td>{serialNumber}</td>
-            </tr>
-            <tr>
-              <th>Terminal Type</th>
-              <td>{terminalType}</td>
-            </tr>
-            <tr>
-              <th>Mobile Number</th>
-              <td>{mobileNumber}</td>
-            </tr>
-            <tr>
-              <th>Mobile Type</th>
-              <td>{mobileType}</td>
-            </tr>
-            <tr>
-              <th>ISN Number</th>
-              <td>{ISNnumber}</td>
-            </tr>
-            <tr>
-              <th>HW Id</th>
-              <td>{HWid}</td>
-            </tr>
-            <tr>
-              <th>Latitude</th>
-              <td>{latitude}</td>
-            </tr>
-            <tr>
-              <th>Longitude</th>
-              <td>{longitude}</td>
-            </tr>
-            <tr>
-              <th>Altitude</th>
-              <td>{altitude}</td>
-            </tr>
-            <tr>
-              <th>GPS Time</th>
-              <td>{GPStime}</td>
-            </tr>
-            <tr>
-              <th>Is Serial Connected</th>
-              <td>{isSerialConnected}</td>
-            </tr>
-            <tr>
-              <th>Host Uptime</th>
-              <td>{hostUptime}</td>
-            </tr>
-            <tr>
-              <th>Application Uptime</th>
-              <td>{applicationUptime}</td>
-            </tr>
-          </table>
+          {jsonData && (
+            <table className="tbl">
+              {Object.entries(jsonData.data).map(([key, value]) => (
+                <tr key={key}>
+                  <th>{key}</th>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </table>
+          )}
         </div>
       </div>
     </div>
