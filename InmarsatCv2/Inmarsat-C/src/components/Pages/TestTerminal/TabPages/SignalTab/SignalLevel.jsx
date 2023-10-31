94464aa6a70991cf377c97d58a6e0ecca9f0fb8d
MuhammadArkan <muhammadarkan@graduate.utm.my>
2023-10-31 13:54:39 +0700

    convert Signaltab datas into json

--- InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/SignalTab/SignalLevel.jsx
+++ InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/SignalTab/SignalLevel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js";
 import {
@@ -10,39 +10,74 @@ import {
   Legend,
 } from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 import "../../../../../App.css";
 
-const epochTimestamp = 1697159325;
-const date = new Date(epochTimestamp * 1000);
-const optionsDate = {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-  hour12: false,
-};
-
 function SignalLevel() {
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/datas/signaltabData/signaltabData.json");
+        const data = await response.json();
+        setJsonData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const epochTimestamp = jsonData?.data?.ts || 1697159325;
+  const date = new Date(epochTimestamp * 1000);
+  const optionsDate = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
   const formattedDate = date.toLocaleDateString("en-US", optionsDate);
 
   const data = {
-    labels: [formattedDate,formattedDate,formattedDate,formattedDate,formattedDate,formattedDate],
+    labels: [
+      formattedDate,
+      formattedDate,
+      formattedDate,
+      formattedDate,
+      formattedDate,
+      formattedDate,
+    ],
     datasets: [
       {
         label: "Signal Strength",
-        data: [5, 5, 5, 5, 5,5],
+        data: [
+          jsonData?.data?.signal,
+          jsonData?.data?.signal,
+          jsonData?.data?.signal,
+          jsonData?.data?.signal,
+          jsonData?.data?.signal,
+          jsonData?.data?.signal,
+        ],
         backgroundColor: "#46B39E",
         stack: "stack1",
         barPercentage: 1,
-        categoryPercentage:1,
+        categoryPercentage: 1,
       },
     ],
   };
-
   const options = {
     plugins: {
       legend: {
