bf5c183ff59a8578e84484e70fd3da89f8037d6a
MuhammadArkan <muhammadarkan@graduate.utm.my>
2023-12-18 02:18:17 +0700

    fix

--- InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/ConfigTab/ConfigTab.jsx
+++ InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/ConfigTab/ConfigTab.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Form, Input, Button } from "antd";
 import PropTypes from "prop-types";
-import axios from 'axios';
+import axios from "axios";
 
 function ConfigTab({ selectedTerminal, activeTab }) {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [isInitialRender, setIsInitialRender] = useState(true);
   const [jsonData, setJsonData] = useState(null);
 
   const fetchJsonData = () => {
@@ -15,16 +14,19 @@ function ConfigTab({ selectedTerminal, activeTab }) {
       return Promise.resolve(null);
     }
 
-    return axios.get(`https://655c2821ab37729791a9ef77.mockapi.io/api/v1/config?dest=${selectedTerminal}`)
-      .then(response => response.data)
-      .catch(error => {
+    return axios
+      .get(
+        `https://655c2821ab37729791a9ef77.mockapi.io/api/v1/config?dest=${selectedTerminal}`
+      )
+      .then((response) => response.data)
+      .catch((error) => {
         throw new Error(`Error fetching data: ${error.message}`);
       });
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!isInitialRender && selectedTerminal !== null) {
+      if (selectedTerminal !== null) {
         setLoading(true);
 
         try {
@@ -37,12 +39,10 @@ function ConfigTab({ selectedTerminal, activeTab }) {
           setLoading(false);
         }
       }
-
-      setIsInitialRender(false);
     };
 
     fetchData();
-  }, [selectedTerminal, isInitialRender, activeTab]);
+  }, [selectedTerminal, activeTab]);
 
   const onFinish = async (values) => {
     try {
@@ -53,7 +53,9 @@ function ConfigTab({ selectedTerminal, activeTab }) {
     } catch (error) {
       console.error("Form submission error:", error);
       if (error.response) {
-        setErrorMessage(`Server responded with an error: ${error.response.status}`);
+        setErrorMessage(
+          `Server responded with an error: ${error.response.status}`
+        );
       } else if (error.request) {
         setErrorMessage("No response received from the server");
       } else {
@@ -64,14 +66,18 @@ function ConfigTab({ selectedTerminal, activeTab }) {
     }
   };
 
+  const initialValues = jsonData?.[0]?.data || {};
+
   return (
-    <Form onFinish={onFinish} form={form} initialValues={jsonData?.data || {}}>
-      {jsonData?.[0]?.data &&
-        Object.entries(jsonData[0].data).map(([key, value]) => (
-          <Form.Item key={key} label={key} name={key}>
-            <Input type={typeof value === 'number' ? 'number' : 'text'} />
-          </Form.Item>
-        ))}
+    <Form onFinish={onFinish} form={form} initialValues={initialValues}>
+      {Object.entries(initialValues).map(([key, value]) => (
+        <Form.Item key={key} label={key} name={key}>
+          <Input
+            type={typeof value === "number" ? "number" : "text"}
+            defaultValue={value}
+          />
+        </Form.Item>
+      ))}
       <Form.Item>
         <Button type="primary" htmlType="submit">
           Submit
