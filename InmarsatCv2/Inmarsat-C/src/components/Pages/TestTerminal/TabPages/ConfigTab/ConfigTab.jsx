0546fc5971bbc18650d5a49268d9b6e6fca49391
MuhammadArkan <muhammadarkan@graduate.utm.my>
2023-12-19 14:08:35 +0700

    fix config tab

--- InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/ConfigTab/ConfigTab.jsx
+++ InmarsatCv2/Inmarsat-C/src/components/Pages/TestTerminal/TabPages/ConfigTab/ConfigTab.jsx
@@ -3,7 +3,6 @@ import { Form, Input, Button } from "antd";
 import PropTypes from "prop-types";
 import "./ConfigTab.css";
 
-
 function ConfigTab({ selectedTerminal, activeTab }) {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
@@ -12,94 +11,64 @@ function ConfigTab({ selectedTerminal, activeTab }) {
   const [isInitialRender, setIsInitialRender] = useState(true);
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       if (activeTab !== "Configtab" || selectedTerminal === null) {
-        return Promise.resolve(null);
+        return;
       }
   
       setLoading(true);
   
-      const xhr = new XMLHttpRequest();
-      xhr.open(
-        "GET",
-        `https://655c2821ab37729791a9ef77.mockapi.io/api/v1/config?dest=${selectedTerminal}`,
-        true
-      );
+      try {
+        const response = await fetch(
+          `https://655c2821ab37729791a9ef77.mockapi.io/api/v1/config?dest=${selectedTerminal}`
+        );
   
-      return new Promise((resolve, reject) => {
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-              const response = JSON.parse(xhr.responseText);
-              resolve(response);
-            } else {
-              reject(new Error(`Network response was not ok: ${xhr.status}`));
-            }
-          }
-        };
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
   
-        xhr.onerror = function () {
-          reject(new Error("XHR request failed"));
-        };
+        const responseData = await response.json();
+        setJsonData(responseData);
   
-        xhr.send();
-      });
+        // Set initial form values using setFieldsValue
+        form.setFieldsValue(responseData?.[0]?.data || {});
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setErrorMessage("An error occurred while fetching data.");
+      } finally {
+        setLoading(false);
+      }
     };
   
-    // Check if it's the initial render
     if (!isInitialRender && selectedTerminal !== null) {
-      fetchData()
-        .then((responseData) => {
-          setJsonData(responseData);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          setErrorMessage("An error occurred while fetching data.");
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+      fetchData();
     }
   
-    // Set isInitialRender to false after the first render
     setIsInitialRender(false);
-  }, [selectedTerminal, isInitialRender, activeTab]);
-  
-
+  }, [selectedTerminal, isInitialRender, activeTab, form]);
+ 
   const onFinish = async (values) => {
     try {
       setLoading(true);
-  
-      // Convert form data to JSON format
-      const formData = JSON.stringify({ data: values });
-  
-      // Create a new XMLHttpRequest object
-      const xhr = new XMLHttpRequest();
-  
-      // Configure it to be a POST request to the desired URL
-      xhr.open("POST", "https://655c2821ab37729791a9ef77.mockapi.io/api/v1/config2", true);
-  
-      // Set the content type to JSON
-      xhr.setRequestHeader("Content-Type", "application/json");
-  
-      // Set the callback function for when the request completes
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          console.log("Form submitted successfully");
-        } else {
-          console.error("Form submission error:", xhr.statusText);
-          setErrorMessage(`Server responded with an error: ${xhr.status}`);
+
+      const formData = { data: values };
+
+      const response = await fetch(
+        "https://655c2821ab37729791a9ef77.mockapi.io/api/v1/config2",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
         }
-      };
-  
-      // Set the callback function for network errors
-      xhr.onerror = function () {
-        console.error("Form submission error: Network error");
-        setErrorMessage("An error occurred. Please try again.");
-      };
-  
-      // Send the request with the form data
-      xhr.send(formData);
+      );
+
+      if (!response.ok) {
+        throw new Error(`Server responded with an error: ${response.status}`);
+      }
+
+      console.log("Form submitted successfully");
     } catch (error) {
       console.error("Form submission error:", error.message);
       setErrorMessage("An error occurred. Please try again.");
@@ -121,18 +90,19 @@ function ConfigTab({ selectedTerminal, activeTab }) {
               key={key}
               label={key}
               name={key}
-              labelCol={{ span: 4 }} // Adjust the span as needed
-              wrapperCol={{ span: 19 }} // Adjust the span as needed
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 19 }}
               className="labelconfig"
             >
-              <Input className="inputconfig"
+              <Input
+                className="inputconfig"
                 type={typeof value === "number" ? "number" : "text"}
                 defaultValue={value}
               />
             </Form.Item>
           ))}
-          <Form.Item wrapperCol={{ offset: 4}}>
-            <Button type="primary" htmlType="submit">
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Button type="primary" htmlType="submit" loading={loading}>
               Submit
             </Button>
           </Form.Item>
